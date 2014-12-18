9dafa27f8114cc66b1b88f36f82160bc6bec4146
Ryabenkiy Sergiy <3dextend@gmail.com>
2014-12-18 13:45:04 +0200

    Add design

--- TestGit/src/HelloGit.java
+++ TestGit/src/HelloGit.java
@@ -4,6 +4,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		
 		System.out.println("Test Git HelloGit!!!");
+		System.out.println("Design by Ryabenkiy Sergiy");
 		// TODO Auto-generated method stub
 
 	}
