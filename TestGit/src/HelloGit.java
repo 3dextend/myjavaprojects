a4eb530572da8df62d81b90a3d66c6566c40e906
Ryabenkiy Sergiy <3dextend@gmail.com>
2014-12-18 14:27:44 +0200

    3-trd commit
    Signed-off-by: Ryabenkiy Sergiy <3dextend@gmail.com>

--- TestGit/src/HelloGit.java
+++ TestGit/src/HelloGit.java
@@ -5,6 +5,7 @@ public class HelloGit {
 		
 		System.out.println("Test Git HelloGit!!!");
 		System.out.println("Design by Ryabenkiy Sergiy");
+		System.out.println("Let's try yet.");
 		// TODO Auto-generated method stub
 
 	}
